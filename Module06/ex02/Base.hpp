99c0fff53da55c43522905bdd17da1e00b79d065
Khalil Haimer <khaimer@c1r6p8.1337.ma>
2024-02-27 14:44:42 +0100

    update

--- Module06/ex02/Base.hpp
+++ Module06/ex02/Base.hpp
@@ -1,19 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Base.hpp                                           :+:      :+:    :+:   */
+/*   base.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: khaimer <khaimer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 16:24:13 by khaimer           #+#    #+#             */
-/*   Updated: 2024/01/30 17:43:23 by khaimer          ###   ########.fr       */
+/*   Updated: 2024/02/27 12:30:21 by khaimer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-class Base
-{
-    public:
-        virtual ~Base();
+class Base {
+public:
+    virtual ~Base() {}
 };
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+
+void identify(Base* p);
+void identify(Base& p);
--- Module06/ex02/Func.cpp
+++ Module06/ex02/Func.cpp
@@ -1,16 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Functions.cpp                                      :+:      :+:    :+:   */
+/*   Func.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: khaimer <khaimer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 16:36:12 by khaimer           #+#    #+#             */
-/*   Updated: 2024/01/30 17:44:08 by khaimer          ###   ########.fr       */
+/*   Updated: 2024/02/27 12:35:01 by khaimer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Functions.hpp"
+#include <iostream>
+#include "Base.hpp"
+#include <cstdlib>
 
 Base* generate(void)
 {
@@ -27,7 +29,34 @@ Base* generate(void)
     return (ptr);
 }
 
+void identify(Base& p) {
+    try {
+        (void)dynamic_cast<A&>(p); std::cout << "A\n";
+    } catch (std::bad_cast&) {}
+    try {
+        (void)dynamic_cast<B&>(p); std::cout << "B\n";
+    } catch (std::bad_cast&) {}
+    try {
+        (void)dynamic_cast<C&>(p); std::cout << "C\n";
+    } catch (std::bad_cast&) {}
+}
+
 void identify(Base* p)
 {
-    
+    if (dynamic_cast<A*>(p)) 
+        std::cout << "A\n";
+    else if (dynamic_cast<B*>(p)) 
+        std::cout << "B\n";
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C\n";                  
+}
+
+int main() 
+{
+    Base *base = generate();
+    identify(base); 
+    identify(*base);
+
+    delete base;
+    return 0;
 }
