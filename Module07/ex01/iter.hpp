95377cdede1c8c03b380e6de4a7a923e64b9e164
Khalil Haimer <khaimer@c1r6p4.1337.ma>
2024-03-21 17:21:21 +0000

    01 finished

--- Module07/ex01/iter.hpp
+++ Module07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: khaimer <khaimer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 17:33:15 by khaimer           #+#    #+#             */
-/*   Updated: 2024/03/21 16:26:38 by khaimer          ###   ########.fr       */
+/*   Updated: 2024/03/21 17:17:31 by khaimer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,6 @@
 template <typename arr, typename F>
 void    iter(arr *array, size_t length, void (*func)(F));
 
-class Awesome
-{
-    public:
-        Awesome( void ) : _n( 42 ) { return; }
-        int get( void ) const { return this->_n; }
-    private:
-        int _n;
-};
 
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) 
-{ 
-    o << rhs.get(); 
-    return o;
-}
-
-template <typename arr>
-void    print(arr &array);
-
-// template< typename T >
-// void print( T const & x ) { std::cout << x << std::endl; return; }
+template <typename F>
+void    print(F &array);
--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: khaimer <khaimer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 17:33:22 by khaimer           #+#    #+#             */
-/*   Updated: 2024/03/21 16:30:32 by khaimer          ###   ########.fr       */
+/*   Updated: 2024/03/21 17:21:03 by khaimer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,24 @@ void    iter(arr *array, size_t length, void (*func)(F))
         func(array[i]);
 }
 
-template <typename arr>
-void    print(arr &array)
+template <typename F>
+void    print(F &array)
 {
     std::cout << "Print Called |" << array << std::endl;
 }
 
 
-
-int main() 
+int main()
 {
-    int tab[] = { 0, 1, 2, 3, 4 };
-    Awesome tab2[5];
-    iter( tab, 5, print );
-    iter( tab2, 5, print );
-    // iter( tab, 5, print<const int> );
-    // iter( tab2, 5, print<Awesome> ); 
-    return 0;
+	int array[] = {1, 2, 3, 4};
+	size_t lenght = 4;
+
+	std::cout << "Before :" << std::endl;
+	for (size_t i = 0 ; i < lenght; i++)
+		std::cout << "[" << array[i] << "]" << std::endl;
+	
+	std::cout << "After :" << std::endl;
+	iter(array, lenght, print<int>);
+    
+	return (0);
 }
