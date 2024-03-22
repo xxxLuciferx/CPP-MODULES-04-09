09f1a536e4cc6ae08442fe4b4ece0917fbeb6760
Khalil Haimer <khaimer@c1r4p4.1337.ma>
2024-03-22 20:38:56 +0000

    update

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -1,25 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: khaimer <khaimer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/21 17:23:28 by khaimer           #+#    #+#             */
-/*   Updated: 2024/03/22 15:52:19 by khaimer          ###   ########.fr       */
+/*   Created: 2024/03/22 17:39:15 by khaimer           #+#    #+#             */
+/*   Updated: 2024/03/22 17:42:01 by khaimer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Array.hpp"
+#include <iostream>
 
-
-int main()
-{
-    Array<int> arr(5);
-    for (std::size_t i = 0; i < arr.len(); i++)
-    {
-        arr[i] = i * 10;
-        std::cout << "arr1[" << i << "] = " << arr[i] << std::endl;
-    }
-    return 0;
-}
+template <typename T>
+void    easyfind(T name, int number);
--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -5,21 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: khaimer <khaimer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/21 17:23:28 by khaimer           #+#    #+#             */
-/*   Updated: 2024/03/22 15:52:19 by khaimer          ###   ########.fr       */
+/*   Created: 2024/03/22 17:39:18 by khaimer           #+#    #+#             */
+/*   Updated: 2024/03/22 17:40:11 by khaimer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Array.hpp"
-
+#include "easyfind.hpp"
 
 int main()
 {
-    Array<int> arr(5);
-    for (std::size_t i = 0; i < arr.len(); i++)
-    {
-        arr[i] = i * 10;
-        std::cout << "arr1[" << i << "] = " << arr[i] << std::endl;
-    }
-    return 0;
+    
 }
