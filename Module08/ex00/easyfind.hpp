e654505c85ac523d48efd7cc2e61c27cda0eeeb6
Khalil Haimer <khaimer@c2r3p4.1337.ma>
2024-03-23 00:05:59 +0000

    updating

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -6,10 +6,11 @@
 /*   By: khaimer <khaimer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 17:39:15 by khaimer           #+#    #+#             */
-/*   Updated: 2024/03/22 17:42:01 by khaimer          ###   ########.fr       */
+/*   Updated: 2024/03/22 22:00:06 by khaimer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 #include <iostream>
 
 template <typename T>
--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -6,13 +6,43 @@
 /*   By: khaimer <khaimer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 17:39:18 by khaimer           #+#    #+#             */
-/*   Updated: 2024/03/22 17:40:11 by khaimer          ###   ########.fr       */
+/*   Updated: 2024/03/22 23:45:22 by khaimer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <iostream>
+
+
+template <typename T>
+T&    easyfind(T *name, int number)
+{
+    // try
+    // {    
+        for (size_t i = 0; i < 4; i++)
+        {
+            if (number == name[i])
+                return name[i];
+        }   
+        // char* khalil = "NOT FOUND";
+        return <T>"NOT FOUND";
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << '\n';
+    // }
+}
+
 
 int main()
 {
+    // int khalil[4] = {1, 3, 20, 18};
+    char khalil[4] = {'0', '3', '2', '9'};
+    int number = 48;
+
+    int result = easyfind(khalil, number);
+    std::cout << result << std::endl;
+    
+    return 0;
     
 }
