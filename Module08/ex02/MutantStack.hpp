b411748ab00bafd3c20a0568ba980172cc85070d
Khalil Haimer <khaimer@c2r3p1.1337.ma>
2024-03-28 20:16:40 +0000

    Close to finish

--- Module08/ex02/MutantStack.hpp
+++ Module08/ex02/MutantStack.hpp
@@ -6,7 +6,30 @@
 /*   By: khaimer <khaimer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 01:49:41 by khaimer           #+#    #+#             */
-/*   Updated: 2024/03/28 01:49:42 by khaimer          ###   ########.fr       */
+/*   Updated: 2024/03/28 17:03:22 by khaimer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <stack>
+#include <iterator>
+
+
+template < typename T>
+class MutantStack : public std::stack<T>
+{
+	private :
+		std::stack<T>  stack_;
+	public :
+		MutantStack() {};
+		~MutantStack() {};
+		typedef typename  std::deque<T>::iterator iterator;
+		MutantStack& operator=(const MutantStack& other)
+		{
+			if (this != &other)
+				this->c = other.c;
+			return (*this);
+		}
+		iterator	begin(void) {return this->c.begin();}
+		iterator	end(void){return this->c.end();}
+};
